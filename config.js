06818a3944ca4e4a4e4da5d0b501d3ff1e469300
shino-petter <125361514+shino-petter@users.noreply.github.com>
2023-02-14 18:11:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    titleWeb: "Tình yêu cụa Băo Bei 🐛 gửi đến Quốc Thịnh ❤️",
+    introTitle: 'Quốc Thịnh ❤️',
+    introDesc: `I would never fall in love again, until I found him
+    I said, “I would never fall, unless it’s you I fall into”
+    I was lost within the darkness, but then I found him
+    I found You`,
+    btnIntro: 'im so glad i found you',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 8 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'dường như chẳng gì có thể đong đếm được tình cảm này, nên bạn chỉ cần biết mình yeeuuuuuuuuuuu bạn dữ luôn ó :>>> hjhj',
+    messDesc: 'nào rảnh cưới mình nha :>>',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
